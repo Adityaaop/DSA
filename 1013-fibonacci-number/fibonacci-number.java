53ae6c4b952e1f529cad65dd6a404ec570f29deb
Adityaaop <adityamishra0578@gmail.com>
2026-03-25 12:36:53 +0530

    Time: 10 ms (27.21%) | Memory: 42.1 MB (44.95%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,29 @@
+	public class Solution {
+
+	    public int fib(int n) {
+
+	        if (n == 0) {
+
+	            return 0;
+
+	        } else if (n == 1) {
+
+	            return 1;
+
+	        } else {
+
+	            return fib(n - 1) + fib(n - 2);
+
+	        }
+
+	    }
+
+	    // Time: O(2^n)
+
+	    // Space: O(n)
+
+	    
+
+	}
+
+	 
